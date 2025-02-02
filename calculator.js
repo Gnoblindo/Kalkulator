ff5f09a1dcbf2560bbb72180d814bd428d619db4
Gnoblindo <szymon.przybyla2010@gmail.com>
2025-02-02 10:27:55 +0100

    one function

--- calculator.js
+++ calculator.js
@@ -2,116 +2,25 @@ let number1 = 0;
 let number2 = 0;
 let operation = 0;
 const plane = document.getElementById("Screen")
-document.getElementById("1").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "1";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 1;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 1;
-    }
+
+const numbers = document.querySelectorAll('.numbers');
+
+numbers.forEach(number => {
+    number.addEventListener('click', function() {
+        const curr = parseInt(this.textContent);
+        plane.innerHTML = plane.innerHTML + curr;
+        if (operation < 1){
+            number1 = number1 * 10;
+            number1 += curr;
+        }
+        else{
+            number2 = number2 * 10;
+            number2 += curr;
+        }
+
+    });
 });
-document.getElementById("2").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "2";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 2;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 2;
-    }
-});
-document.getElementById("3").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "3";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 3;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 3;
-    }
-});
-document.getElementById("4").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "4";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 4;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 4;
-    }
-});
-document.getElementById("5").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "5";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 5;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 5;
-    }
-});
-document.getElementById("6").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "6";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 6;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 6;
-    }
-});
-document.getElementById("7").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "7";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 7;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 7;
-    }
-});
-document.getElementById("8").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "8";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 8;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 8;
-    }
-});
-document.getElementById("9").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "9";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 9;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 9;
-    }
-});
-document.getElementById("zero").addEventListener('click', function(){
-    plane.innerHTML = plane.innerHTML + "0";
-    if (operation < 1){
-        number1 = number1 * 10;
-        number1 += 0;
-    }
-    else{
-        number2 = number2 * 10;
-        number2 += 0;
-    }
-});
+
 document.getElementById("operation+").addEventListener('click', function(){
     plane.innerHTML = plane.innerHTML + "+";
     operation += 1;
